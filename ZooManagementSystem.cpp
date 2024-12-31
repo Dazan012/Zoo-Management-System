d83e3437ae6178bf7005044565c79d4435e3a02b
Dazan012 <dazanclassico12@gmail.com>
2024-12-31 14:53:18 +0300

    Animal class

--- ZooManagementSystem.cpp
+++ ZooManagementSystem.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+class Animal
+{
+public:
+    Animal(std::string name, std::string species) : name(name), species(species) {}
+    void displayInfo()
+    {
+        std::cout << "Name: " << name << ", Species: " << species << std::endl;
+    }
+
+private:
+    std::string name;
+    std::string species;
+};
